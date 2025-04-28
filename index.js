964c42abc5b0f9bfbb249de5645408c52ae431c2
Thspli <thalissondouglas86@gmail.com>
2025-04-28 15:42:05 -0300

    API

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(helmet());
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+    }
+    next();
+  });
+
+  module.exports = app; // Exporta o app para ser usado no server.js
