1602f365a67a21f2fb687c04a3e2c09c3d7b7a75
Thspli <thalissondouglas86@gmail.com>
2025-05-26 16:27:38 -0300

    ddl pra livia

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -55,37 +55,45 @@ Values (?, ?, ?, ?, ?, ?);`,
 }
 
 exports.login = async (req, res) => {
-    try{
+    try {
         const usuario = await mysql.execute(
             `SELECT * FROM users WHERE email = ?;`,
             [req.body.email]);
 
-            if(usuario.lenght == 0){
-                return res.status(401).send({ "Mensagem": "Usuario não encontrado!" })
-            }
+        if (usuario.lenght == 0) {
+            return res.status(401).send({ "Mensagem": "Usuario não encontrado!" })
+        }
 
 
-            const match = await bcrypt.compare(req.body.password, usuario[0].password);
-            if(!match){
-                return res.status(401).send({ "Mensagem": "Senha incorreta!" })
-            }
+        const match = await bcrypt.compare(req.body.password, usuario[0].password);
+        if (!match) {
+            return res.status(401).send({ "Mensagem": "Senha incorreta!" })
+        }
 
 
-            const token = jwt.sign({
+        const token = jwt.sign({
+            id: usuario[0].id,
+            first_name: usuario[0].first_name,
+            last_name: usuario[0].last_name,
+            email: usuario[0].email,
+            birth_date: usuario[0].birth_date,
+            phone: usuario[0].phone,
+            admin: usuario[0].admin
+        }, "senhadojwt");
+
+        return res.status(200).send({
+            "Mensagem": "Login realizado com sucesso!",
+            "token": token,
+            "user": {
                 id: usuario[0].id,
                 first_name: usuario[0].first_name,
                 last_name: usuario[0].last_name,
                 email: usuario[0].email,
                 birth_date: usuario[0].birth_date,
                 phone: usuario[0].phone,
-                admin: usuario[0].admin
-            },"senhadojwt");
-
-            return res.status(200).send({
-                "Mensagem": "Login realizado com sucesso!",
-                "token": token
-            });
-    }catch(error){
+            }
+        });
+    } catch (error) {
         return res.status(500).send({ "Mensagem": error })
     }
 }
