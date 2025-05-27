78215d0b59b62a0667c2acba9177433190141232
Thspli <thalissondouglas86@gmail.com>
2025-05-27 11:15:51 -0300

    Sistema de Biston

--- controllers/brinquedos.controller.js
+++ controllers/brinquedos.controller.js
@@ -19,3 +19,25 @@ exports.cadastrarBrinquedo = async (req, res) => {
         return res.status(500).send(error);
     }
 }
+
+
+exports.getBrinquedosByAreaName = async (req, res) => {
+    try {
+        resultados = await mysql.execute(`SELECT * FROM rides WHERE id_areas = (SELECT id from areas where name = ?);`, [req.params.areaName]);
+
+        if (resultados.length == 0) {
+            return res.status(404).send({
+                "Mensagem": "Nenhum brinquedo encontrado para a área especificada."
+            });
+        }
+        return res.status(200).send({
+            "Mensagem": "Brinquedos encontrados com sucesso!",
+            "Resultado": resultados
+        });
+    } catch (error) {
+        return res.status(500).send({
+            "Mensagem": "Erro ao buscar brinquedos por área.",
+            "Erro": error
+        });
+    }
+}
--- routes/brinquedos.route.js
+++ routes/brinquedos.route.js
@@ -6,4 +6,7 @@ const brinquedosController = require("../controllers/brinquedos.controller");
 
 router.post("/", login.required, login.userRequired, brinquedosController.cadastrarBrinquedo);
 
+router.get("/area/:areaName", login.required, brinquedosController.getBrinquedosByAreaName);
+
 module.exports = router;
+
